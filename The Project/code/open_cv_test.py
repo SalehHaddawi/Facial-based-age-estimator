d98627377dc370432088cba5081459bf04eb6d55
saleh haddawi <saleh.haddawi@gmail.com>
2019-12-03 17:28:58 +0300

    fix imread can't read image with non english letters

--- The Project/code/open_cv_test.py
+++ The Project/code/open_cv_test.py
@@ -1,80 +1,90 @@
 from FacialBasedAgeEstimator import FacialBasedAgeEstimator
 import cv2
+import numpy
 from tkinter import *
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, askopenfile
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+fbae_image = FacialBasedAgeEstimator(face_cascade, 1.05)
 
 
-
-
-face_cascade = cv2.CascadeClassifier('The Project/code/haarcascade_frontalface_default.xml')
+fbae_vide_cam = FacialBasedAgeEstimator(face_cascade, 1.15)
 
 
 def get_image():
-    image = askopenfilename(filetypes=[("Image", ".jpg .png .jpeg")])
-    print(image)
-    scaleFactor = 1.05
+    path = askopenfile(filetypes=[("Image", ".jpg .png .jpeg")]).name
+    # scaleFactor = 1.05
+    #
+    # fbae = FacialBasedAgeEstimator(face_cascade, scaleFactor)
 
-    fbae = FacialBasedAgeEstimator(face_cascade, scaleFactor)
-
-    img = cv2.imread(image)
+    stream = open(path, "rb")
+    bytes = bytearray(stream.read())
+    numpyarray = numpy.asarray(bytes, dtype=numpy.uint8)
+    bgrImage = cv2.imdecode(numpyarray, cv2.IMREAD_UNCHANGED)
 
     cv2.waitKey(0)
-    result = fbae.predict_image(img)
+    result = fbae_image.predict_image(bgrImage)
 
     cv2.imshow("img", result)
 
     cv2.waitKey(0)
 
+
 def get_video():
     video = askopenfilename(filetypes=[("Image", ".mp4 .mkv")])
-    print(video)
-    scaleFactor = 1.15
-    fbae = FacialBasedAgeEstimator(face_cascade, scaleFactor)
-    fbae.predict_video(source=video, sync=True)
+    # scaleFactor = 1.15
+    # fbae = FacialBasedAgeEstimator(face_cascade, scaleFactor)
+    fbae_vide_cam.predict_video(source=video, sync=True)
+
 
 def open_cam():
     scaleFactor = 1.15
     fbae = FacialBasedAgeEstimator(face_cascade, scaleFactor)
     fbae.predict_video(source=0, sync=True)
 
-
-root = Tk() 
-frame = Frame(root) 
-frame.pack() 
-bottomframe = Frame(root) 
-bottomframe.pack( side = BOTTOM ) 
-redbutton = Button(frame, text = 'Load Image', fg ='black',command=get_image) 
-redbutton.pack( side = TOP) 
-greenbutton = Button(frame, text = 'Load Video', fg='black',command=get_video) 
-greenbutton.pack( side = TOP ) 
-bluebutton = Button(frame, text ='Web Cam', fg ='black',command=open_cam) 
-bluebutton.pack( side = TOP ) 
-root.mainloop() 
+def close_windows():
+    fbae_image.close_image_video_cam()
 
 
+root = Tk()
+frame = Frame(root)
+frame.pack()
+bottomframe = Frame(root)
+bottomframe.pack(side=BOTTOM)
 
+open_image_button = Button(frame, text='Load Image', fg='black', command=get_image)
+open_image_button.pack(side=TOP)
 
+open_video_button = Button(frame, text='Load Video', fg='black', command=get_video)
+open_video_button.pack(side=TOP)
 
+web_cam_button = Button(frame, text='Web Cam', fg='black', command=open_cam)
+web_cam_button.pack(side=TOP)
 
+close_button = Button(frame, text='Close', fg='black', command=close_windows())
+close_button.pack(side=TOP)
+
+root.mainloop()
 
 # # ------------------------------ MAIN -------------------------------
 
-# # load the module
-# face_cascade = cv2.CascadeClassifier('The Project/code/haarcascade_frontalface_default.xml')
-
-# # ------------ STATIC IMAGES --------------
-# # scaleFactor = 1.05
-
-# # fbae = FacialBasedAgeEstimator(face_cascade, scaleFactor)
-
-# # img = cv2.imread("The Project/code/baby.jpeg")
-
-# # cv2.waitKey(0)
-# # result = fbae.predict_image(img)
-
-# # cv2.imshow("img", result)
-
-# # cv2.waitKey(0)
+# load the module
+# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#
+# # # ------------ STATIC IMAGES --------------
+# scaleFactor = 1.05
+#
+# fbae = FacialBasedAgeEstimator(face_cascade, scaleFactor)
+#
+# img = cv2.imread("family.jpg")
+#
+# cv2.waitKey(0)
+# result = fbae.predict_image(img)
+#
+# cv2.imshow("img", result)
+#
+# cv2.waitKey(0)
 
 # # ------------ VIDEOS & WEB CAM --------------
 # # scaleFactor = 1.15
